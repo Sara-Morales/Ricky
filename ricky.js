16263ef071ca6b0c96fd6f2a1ed6d7842be19054
Luisa Ramirez C <luisaf.ramirez.cardona11@gmail.com>
2023-06-24 09:37:55 -0500

    javascriptApi

--- ricky.js
+++ ricky.js
@@ -1,1 +1,17 @@
-alert("Hola");
+let json;
+async function loadCharacters(url) {
+  const $containerCharacters = document.querySelector("show-characters");
+  let RickyAPI = "https://rickandmortyapi.com/api/character";
+  try {
+    const res = await fetch(RickyAPI);
+    json = await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+document.addEventListener("DOMContentLoaded", loadCharacters);
+document.addEventListener("click", (e) => {
+  if (e.target.matches(".show-characters")) {
+    console.log(json);
+  }
+});
