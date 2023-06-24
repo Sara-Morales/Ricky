8fc6f701f5f6583593c36f1b9e07ad7432603f5e
Luisa Ramirez C <luisaf.ramirez.cardona11@gmail.com>
2023-06-24 11:15:11 -0500

    javascript

--- ricky.js
+++ ricky.js
@@ -1,17 +1,37 @@
+const $containerCharacters = document.querySelector(".container-characters");
 let url = "https://rickandmortyapi.com/api/character";
-const $containerCharacters = document.querySelector("show-characters");
+
 async function loadCharacters(url) {
   try {
-    const res = await fetch(url);
-    const json = await res.json();
-    return json;
+    let res = await fetch(url);
+    let json = await res.json();
+    let $template = "";
+
+    //console.log(res);
+    //console.log(json);
+
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
+
+    for (let i = 0; i < json.results.length; i++) {
+      //console.log(json.results[i]);
+      $template += `
+        <div class="card-container" >
+            <div class="card-image">
+              <img class="" src="${json.results[i].image}">
+            </div>
+            <div class="card-name">
+              <p><b>${json.results[i].name}</b></p>
+            </div>
+            <div class="card-details">
+             <p class="">${json.results[i].species},&nbsp${json.results[i].gender},&nbsp${json.results[i].status}<b></b></p>
+            </div> 
+          </div>
+        `;
+      $containerCharacters.appendChild($template);
+    }
   } catch (err) {
     console.log(err);
   }
 }
-document.addEventListener("click", async (e) => {
-  const characters = await loadCharacters(url);
-  if (e.target.matches(".show-characters")) {
-    console.log(characters);
-  }
-});
+
+loadCharacters(url);
